a0414f71764a2cda29717191c4edc5aded73a568
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2019-12-11 15:13:55 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ function App() {
   return (
     <div className="App">
       <NasaCard />
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun !
-      </p>
     </div>
   );
 }
