9087a6c98aaceb4f1303cb2115ae4019a19e9e88
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2019-12-14 13:48:26 -0700

    mobile updae

--- src/components/Image.js
+++ src/components/Image.js
@@ -11,7 +11,9 @@ export default function ImageDiv (props){
         border-radius: 10px;
         margin: 2%;
         box-shadow: 10px 10px 10px yellow;
-   
+        @media (max-width: 500px){
+            width: 50%;
+        }
 `;
 
     return(
